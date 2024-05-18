941e4e88f47d6e93455978c43f289961baf46fc5
Ultimatu <tondesoulco@gmail.com>
2024-05-18 18:06:51 +0000

    update

--- app/Filament/Resources/VisitorResource/Widgets/VisitorStats.php
+++ app/Filament/Resources/VisitorResource/Widgets/VisitorStats.php
@@ -16,7 +16,7 @@ class VisitorStats extends BaseWidget
                 ->icon('heroicon-s-eye')
                 ->chartColor('primary')
                 //chart take float array, sum visitor by day
-                ->chart([4, 5, 6, 7, 8, 9, 10])
+                ->chart($this->getVisitorByDay())
                 ->description('Nombre total de visiteurs uniques')
                 ->descriptionIcon('heroicon-s-information-circle')
                 ->descriptionColor('info')
@@ -25,12 +25,14 @@ class VisitorStats extends BaseWidget
             Stat::make('Total Visites', Visitor::sum('visits'))
                 ->icon('heroicon-s-eye')
                 ->description('Nombre total de visites sur le site')
+                ->chart($this->getVisitsByDay())
                 ->descriptionIcon('heroicon-s-information-circle')
                 ->descriptionColor('info')
                 ->color('success'),
             //this year
             Stat::make('Visiteurs Cette Année', Visitor::whereYear('created_at', now())->count())
                 ->icon('heroicon-s-eye')
+                ->chart($this->getYearVisitorByMonth())
                 ->description('Nombre de visiteurs cette année')
                 ->descriptionIcon('heroicon-s-information-circle')
                 ->descriptionColor('info')
@@ -38,6 +40,7 @@ class VisitorStats extends BaseWidget
             // this month 
             Stat::make('Visiteurs Ce Mois', Visitor::whereMonth('created_at', now())->count())
                 ->icon('heroicon-s-eye')
+                ->chart($this->getMonthVisitorByDay())
                 ->description('Nombre de visiteurs ce mois')
                 ->descriptionIcon('heroicon-s-information-circle')
                 ->descriptionColor('danger')
@@ -46,6 +49,7 @@ class VisitorStats extends BaseWidget
             // this week
             Stat::make('Visiteurs Cette Semaine', Visitor::whereBetween('created_at', [now()->startOfWeek(), now()->endOfWeek()])->count())
                 ->icon('heroicon-s-eye')
+                ->chart($this->getWeekVisitorByDay())
                 ->description('Nombre de visiteurs cette semaine')
                 ->descriptionIcon('heroicon-s-information-circle')
                 ->descriptionColor('warning')
@@ -53,6 +57,7 @@ class VisitorStats extends BaseWidget
             // today
             Stat::make('Visiteurs Aujourd\'hui', Visitor::whereDate('created_at', now())->count())
                 ->icon('heroicon-s-eye')
+                ->chart($this->getTodayVisitor())
                 ->description('Nombre de visiteurs aujourd\'hui')
                 ->descriptionIcon('heroicon-s-information-circle')
                 ->descriptionColor('warning')
@@ -66,8 +71,117 @@ class VisitorStats extends BaseWidget
         $visitorByDay = Visitor::selectRaw('DATE(created_at) as date, count(id) as total')
             ->groupBy('date')
             ->get()
-            ->pluck('total', 'date')
+            ->pluck('total')
             ->toArray();
         return $visitorByDay;
+    }
+
+    protected function getVisitsByDay(): array
+    {
+        $visitsByDay = Visitor::selectRaw('DATE(created_at) as date, sum(visits) as total')
+            ->groupBy('date')
+            ->get()
+            ->pluck('total')
+            ->toArray();
+        return $visitsByDay;
+    }
+
+    protected function getYearVisitorByMonth(): array
+    {
+        $visitorByMonth = Visitor::selectRaw('MONTH(created_at) as month, count(id) as total')
+            ->whereYear('created_at', now())
+            ->groupBy('month')
+            ->get()
+            ->pluck('total')
+            ->toArray();
+        return $visitorByMonth;
+    }
+
+    protected function getMonthVisitorByDay(): array
+    {
+        $visitorByDay = Visitor::selectRaw('DAY(created_at) as day, count(id) as total')
+            ->whereMonth('created_at', now())
+            ->groupBy('day')
+            ->get()
+            ->pluck('total')
+            ->toArray();
+        return $visitorByDay;
+    }
+
+    protected function getWeekVisitorByDay(): array
+    {
+        $visitorByDay = Visitor::selectRaw('DAY(created_at) as day, count(id) as total')
+            ->whereBetween('created_at', [now()->startOfWeek(), now()->endOfWeek()])
+            ->groupBy('day')
+            ->get()
+            ->pluck('total')
+            ->toArray();
+        return $visitorByDay;
+    }
+
+    protected function getTodayVisitor(): array
+    {
+        $visitorByDay = Visitor::selectRaw('HOUR(created_at) as hour, count(id) as total')
+            ->whereDate('created_at', now())
+            ->groupBy('hour')
+            ->get()
+            ->pluck('total')
+            ->toArray();
+        return $visitorByDay;
+    }
+
+    protected function getYesterdayVisitor(): array
+    {
+        $visitorByDay = Visitor::selectRaw('HOUR(created_at) as hour, count(id) as total')
+            ->whereDate('created_at', now()->subDay())
+            ->groupBy('hour')
+            ->get()
+            ->pluck('total')
+            ->toArray();
+        return $visitorByDay;
+    }
+
+    protected function getThisMonthVisitor(): array
+    {
+        $visitorByDay = Visitor::selectRaw('DAY(created_at) as day, count(id) as total')
+            ->whereMonth('created_at', now())
+            ->groupBy('day')
+            ->get()
+            ->pluck('total')
+            ->toArray();
+        return $visitorByDay;
+    }
+
+    protected function getLastMonthVisitor(): array
+    {
+        $visitorByDay = Visitor::selectRaw('DAY(created_at) as day, count(id) as total')
+            ->whereMonth('created_at', now()->subMonth())
+            ->groupBy('day')
+            ->get()
+            ->pluck('total')
+            ->toArray();
+        return $visitorByDay;
+    }
+
+    protected function getThisYearVisitor(): array
+    {
+        $visitorByMonth = Visitor::selectRaw('MONTH(created_at) as month, count(id) as total')
+            ->whereYear('created_at', now())
+            ->groupBy('month')
+            ->get()
+            ->pluck('total')
+            ->toArray();
+        return $visitorByMonth;
+    }
+
+    protected function getLastYearVisitor(): array
+    {
+        $visitorByMonth = Visitor::selectRaw('MONTH(created_at) as month, count(id) as total')
+            ->whereYear('created_at', now()->subYear())
+            ->groupBy('month')
+            ->get()
+            ->pluck('total')
+            ->toArray();
+        return $visitorByMonth;
     }
 }
