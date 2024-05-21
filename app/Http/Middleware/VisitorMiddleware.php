2f4b7c5239fead5fe8a33195d7b64bfd05777967
Ultimatu <tondesoulco@gmail.com>
2024-05-21 00:48:54 +0000

    update

--- app/Http/Middleware/VisitorMiddleware.php
+++ app/Http/Middleware/VisitorMiddleware.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Middleware;
 
-
 use App\Models\Visitor;
 use Closure;
 use GeoIp2\Database\Reader;
@@ -12,6 +11,19 @@ use Carbon\Carbon;
 
 class VisitorMiddleware
 {
+    protected $botAgents = [
+        'googlebot', 'bingbot', 'slurp', 'duckduckbot', 'baiduspider', 'yandexbot', 'sogou', 'exabot', 
+        'facebot', 'ia_archiver', 'mj12bot', 'semrushbot', 'ahrefsbot', 'spbot', 'pinterestbot', 'crawler',
+        'yahoo', 'bot', 'spider', 'archive.org_bot', 'applebot', 'linkedinbot', 'embedly', 'quora link preview',
+        'showyoubot', 'outbrain', 'wkhtmltoimage', 'wkhtmltopdf', 'semrushbot', 'ltx71', 'screaming frog',
+        'facebookexternalhit', 'monit', 'dataprovider', 'Xenu', 'Domain re-animator', 'SeznamBot', 'Seznam screenshot-generator',
+        'Google Web Preview', 'Pinterest', 'Mediatoolkitbot', 'Slackbot', 'vkShare', 'TelegramBot', 'Discordbot',
+        'WhatsApp', 'SerendeputyBot', 'Baiduspider', 'BingPreview', 'CensysInspect', 'NetcraftSurveyAgent',
+        'Page2RSS', 'CCBot', 'IstellaBot', 'Mail.RU_Bot', 'Python-urllib', 'DotBot', 'Googlebot-Image', 'Googlebot-Video',
+        'AdsBot-Google', 'Googlebot-News', 'Google-Site-Verification', 'GoogleSecurityScanner'
+    ];
+
+
     /**
      * Handle an incoming request.
      *
@@ -19,54 +31,69 @@ class VisitorMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if (config('app.env') === 'production') {
-            $ip_address = $request->ip();
-            $reader = new Reader(storage_path('app/geoip/GeoLite2-City.mmdb'), ['fr']);
-            $record = $reader->city($ip_address);
-            $country = $record->country->name ?? null;
-            $city = $record->city->name ?? null;
-            $region = $record->mostSpecificSubdivision->name ?? null;
-            $postal_code = $record->postal->code ?? null;
-            $user_agent = $request->header('User-Agent');
-            $session_id = $request->session()->getId();
-
-            $visitor = Visitor::where('ip_address', $ip_address)->where('session_id', $session_id)->first();
-
-            if (!$visitor || $this->shouldUpdateVisitor($visitor->last_visit)) {
-                Visitor::updateOrCreate(
-                    [
-                        'ip_address' => $ip_address,
-                        'session_id' => $session_id,
-                    ],
-                    [
-                        'country' => $country,
-                        'city' => $city,
-                        'region' => $region,
-                        'postal_code' => $postal_code,
-                        'user_agent' => $user_agent,
-                        'last_visit' => Carbon::now()
-                    ]
-                );
-            }
-        } else {
-            $ip = $request->ip();
-            $session_id = $request->session()->getId();
-            $visitor = Visitor::where('ip_address', $ip)->where('session_id', $session_id)->first();
-
-            if (!$visitor || $this->shouldUpdateVisitor($visitor->last_visit)) {
-                Visitor::updateOrCreate(
-                    [
-                        'ip_address' => $ip,
-                        'session_id' => $session_id,
-                    ],
-                    [
-                        'last_visit' => Carbon::now()
-                    ]
-                );
-            }
+        if ($this->isBot($request->header('User-Agent'))) {
+            return $next($request);
         }
 
+        if (config('app.env') === 'production') {
+            $this->handleProduction($request);
+        } else {
+            $this->handleNonProduction($request);
+        }
+
+       
         return $next($request);
+    }
+
+    private function handleProduction(Request $request)
+    {
+        $ip_address = $request->ip();
+        $reader = new Reader(storage_path('app/geoip/GeoLite2-City.mmdb'), ['fr']);
+        $record = $reader->city($ip_address);
+        $country = $record->country->name ?? null;
+        $city = $record->city->name ?? null;
+        $region = $record->mostSpecificSubdivision->name ?? null;
+        $postal_code = $record->postal->code ?? null;
+        $user_agent = $request->header('User-Agent');
+        $session_id = $request->session()->getId();
+
+        $visitor = Visitor::where('ip', $ip_address)->where('session_id', $session_id)->first();
+
+        if (!$visitor || $this->shouldUpdateVisitor($visitor->last_visit)) {
+            Visitor::updateOrCreate(
+                [
+                    'ip' => $ip_address,
+                    'session_id' => $session_id,
+                ],
+                [
+                    'country' => $country,
+                    'city' => $city,
+                    'region' => $region,
+                    'postal_code' => $postal_code,
+                    'user_agent' => $user_agent,
+                    'last_visit' => Carbon::now()
+                ]
+            );
+        }
+    }
+
+    private function handleNonProduction(Request $request)
+    {
+        $ip = $request->ip();
+        $session_id = $request->session()->getId();
+        $visitor = Visitor::where('ip', $ip)->where('session_id', $session_id)->first();
+
+        if (!$visitor || $this->shouldUpdateVisitor($visitor->last_visit)) {
+            Visitor::updateOrCreate(
+                [
+                    'ip' => $ip,
+                    'session_id' => $session_id,
+                ],
+                [
+                    'last_visit' => Carbon::now()
+                ]
+            );
+        }
     }
 
     private function shouldUpdateVisitor($lastVisit)
@@ -75,7 +102,17 @@ class VisitorMiddleware
             return true;
         }
 
-        // Update if last visit was more than 30 minutes ago
-        return Carbon::now()->diffInMinutes($lastVisit) > 1;
+        // Update if last visit was more than 15 minutes ago
+        return Carbon::now()->diffInMinutes($lastVisit) > 15;
+    }
+
+    private function isBot($userAgent)
+    {
+        foreach ($this->botAgents as $botAgent) {
+            if (stripos($userAgent, $botAgent) !== false) {
+                return true;
+            }
+        }
+        return false;
     }
 }
